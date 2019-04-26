31bc915636b770c712000e0d6e090e44dd55d273
Tomasz <kontakt@tkadziolka.pl>
2019-04-26 17:51:35 +0200

    Refactored GsonUtils and SharedPrefsHelper.

--- app/src/main/java/com/czterysery/MVPWithFirebase/util/SharedPrefsHelper.kt
+++ app/src/main/java/com/czterysery/MVPWithFirebase/util/SharedPrefsHelper.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import android.content.SharedPreferences
 import org.jetbrains.anko.defaultSharedPreferences
 
+/*
+    This class manages SharedPreferences in the app.
+    This is modified example from:
+    https://stackoverflow.com/a/53240443
+ */
 class SharedPrefsHelper(context: Context) {
 
     private var prefs: SharedPreferences = context.defaultSharedPreferences
@@ -12,22 +17,10 @@ class SharedPrefsHelper(context: Context) {
         return prefs.getString(key, value)
     }
 
-    fun read(key: String, value: Long): Long? {
-        return prefs.getLong(key, value)
-    }
-
     fun write(key: String, value: String) {
         val prefsEditor: SharedPreferences.Editor = prefs.edit()
         with(prefsEditor) {
             putString(key, value)
-            apply()
-        }
-    }
-
-    fun write(key: String, value: Long) {
-        val prefsEditor: SharedPreferences.Editor = prefs.edit()
-        with(prefsEditor) {
-            putLong(key, value)
             apply()
         }
     }
